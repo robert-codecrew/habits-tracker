14fc2f1c0265338a451a226e9d5ce1b0af06543f
Robert Armstrong <robert@code-crew.org>
2022-06-15 09:45:21 -0500

    pushing new refs

--- client/habit-tracking-app/src/App.js
+++ client/habit-tracking-app/src/App.js
@@ -1,21 +1,22 @@
-import axios from 'axios'
+
+import './App.css';
+
 function App() {
+
   const url = process.env.REACT_APP_SERVER_URL || `http://localhost:3001/allhabits`
-  const getHabits = async( ) => {
+  const getHabits = async () => {
     let res = await axios.get(url)
-    console.log(res.data)
+
+    return res.data
 
   }
   getHabits()
 
-
-
   return (
     <div className="App">
       <header className="App-header">
-       Habitual
+        Habitual
       </header>
-      
     </div>
   );
 }
