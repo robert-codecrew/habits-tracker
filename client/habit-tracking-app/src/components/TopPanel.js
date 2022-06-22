df8b7e23460c2208fddcea6b5461ad3fc50122a3
Robert Armstrong <robert@code-crew>
2022-06-22 10:42:42 -0500

    pulling

--- client/habit-tracking-app/src/components/TopPanel.js
+++ client/habit-tracking-app/src/components/TopPanel.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+
 export default class TopPanel extends Component {
   render() {
     return (
         <div id = 'top-panel'>
-            #topPanel
+          
+            search bar
             
         </div>
     )
